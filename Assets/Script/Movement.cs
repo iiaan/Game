e38221012130db53abca66e07e31459f10185e72
iiaan <ianjosejf@gmail.com>
2024-02-09 22:00:27 +0100

    Animacion personaje

--- Assets/Script/Movement.cs
+++ Assets/Script/Movement.cs
@@ -5,12 +5,17 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     private Rigidbody2D Rigidbody2D;
+    private Animator animator;
+    public bool Left;
+    public bool Right;
+    public bool Default;
     private float Horizontal;
     private float Vertical;
 
     // Start is called before the first frame update
     void Start()
     {
+        animator = GetComponent<Animator>();
         Rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
@@ -19,7 +24,47 @@ public class Movement : MonoBehaviour
     {
         Horizontal = Input.GetAxisRaw("Horizontal");
         Vertical = Input.GetAxisRaw("Vertical");
-        if (Horizontal < -1) { }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            animator.SetBool("Left", true);
+            animator.SetBool("Default", false);
+        }
+        else if (Input.GetKeyUp(KeyCode.A))
+        {
+            animator.SetBool("Left", false);
+            animator.SetBool("Default", true);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            animator.SetBool("Right", true);
+            animator.SetBool("Default", false);
+        }
+        else if (Input.GetKeyUp(KeyCode.D))
+        {
+            animator.SetBool("Right", false);
+            animator.SetBool("Default", true);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            animator.SetBool("Down", true);
+            animator.SetBool("Default", false);
+        }
+        else if (Input.GetKeyUp(KeyCode.S))
+        {
+            animator.SetBool("Down", false);
+            animator.SetBool("Default", true);
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            animator.SetBool("Up", true);
+            animator.SetBool("Default", false);
+        }
+        else if (Input.GetKeyUp(KeyCode.W))
+        {
+            animator.SetBool("Up", false);
+            animator.SetBool("Default", true);
+        }
     }
 
     private void FixedUpdate()
